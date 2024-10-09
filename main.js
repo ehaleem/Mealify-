bf2c6e267927cae231ba137ed719f42dce8718c0
Abd_Elhaleem <73347828+ehaleem@users.noreply.github.com>
2024-10-09 13:57:19 +0300

    done

---
+++ main.js
@@ -0,0 +1,11 @@
+let icon = document.getElementById("icon")
+let menu = document.getElementById("menu")
+
+
+icon.addEventListener("click",function(){
+   
+   menu.classList.toggle("h-s")
+   
+   
+
+})
